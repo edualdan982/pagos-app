86c3de7be41be730b78932bfc6636054d20e4f19
edsarmiento1 <edsarmiento1@umsa.bo>
2024-06-13 18:51:43 -0400

    Agrengaod el for al router link

--- frontend/oauth-app/src/app/shared/side-nav/side-nav.component.ts
+++ frontend/oauth-app/src/app/shared/side-nav/side-nav.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-side-nav',
@@ -11,4 +12,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './side-nav.component.html',
   styles: ``,
 })
-export class SideNavComponent {}
+export class SideNavComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+
+  constructor() {}
+}
