d696f8f4820bc7ac76cc6c378c056e1ff6b19455
edsarmiento1 <edsarmiento1@umsa.bo>
2024-06-14 16:50:13 -0400

    Pruebas y cambios

--- frontend/oauth-app/src/app/auth/pages/auth.component.ts
+++ frontend/oauth-app/src/app/auth/pages/auth.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { MatGridListModule } from '@angular/material/grid-list';
 import { SideNavComponent } from '../../shared';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [
     MatGridListModule,
+    RouterOutlet,
     SideNavComponent
   ],
   templateUrl: './auth.component.html',
--- frontend/oauth-app/src/app/shared/side-nav/side-nav.component.ts
+++ frontend/oauth-app/src/app/shared/side-nav/side-nav.component.ts
@@ -4,20 +4,32 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './side-nav.component.html',
   styles: ``,
 })
 export class SideNavComponent {
   public menuItems = routes
-    .map((route) => route.children ?? [])
-    .flat()
-    .filter((route) => route && route.path)
-    .filter((route) => !route.path?.includes(':'));
+      .map((route) => route?.children ?? [])
+      .flat()
+      .filter((route) => route && route?.path)
+      .filter((route) => !route.path?.includes(':'));
 
-  constructor() {}
+  constructor() {
+    console.log(routes);
+    console.log(routes.map((route) => route?.children ?? []));
+    console.log(routes.map((route) => route?.children ?? []).flat());
+  }
 }
